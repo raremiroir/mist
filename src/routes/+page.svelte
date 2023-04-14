a992ab8cf51528f7c340ac6054aeca1bf3c803d3
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-15 01:53:12 +0200

    Add test content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,10 @@
-<div class="bg-success">
+<script>
+	import Button from "$src/lib/components/Common/Button/Button.svelte";
+
+</script>
+<div class="m-flex m-center bg-red-500 bg-opacity-80">
    <h1 class="text-title-2xl font-">Page</h1>
+   <div>smthni</div>
 </div>
+
+<Button>Button</Button>
