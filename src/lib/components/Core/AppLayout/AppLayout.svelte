15baf8392743bf5b474653164d3dbae57d241736
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-14 23:25:58 +0200

    Add Layout component

---
+++ src/lib/components/Core/AppLayout/AppLayout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+   import '$src/app.postcss'
+
+   import gen from '$lib/theme/generator';
+
+   // const myColors = gen.color.obj([
+   //    {color: '#54648e', name: 'primary'}, 
+   //    {color: '#54818e', name: 'secondary'}, 
+   //    {color: '#005544', name: 'tertiary'}]
+   //    );
+   // console.log(myColors);
+
+   const box = gen.box.full();
+   console.log(box);
+   
+</script>
+
+<slot/>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { AppLayout } from '$lib/components';
+</script>
+
+<AppLayout>
+	<slot />
+</AppLayout>
