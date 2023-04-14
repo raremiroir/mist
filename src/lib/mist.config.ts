8f641b25de334fdd1d47425705fc709ec52c08db
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-15 00:02:41 +0200

    Create config file for theming

---
+++ src/lib/mist.config.ts
@@ -0,0 +1,37 @@
+
+
+export const mist_config = {
+   theme: {
+      // Colors
+      colors: {
+         primary: "#14b8a6",
+         secondary: "#F34213",
+         tertiary: "#0891b2",
+         success: "#65a30d",
+         warning: "#fbbf24",
+         error: "#dc2626",
+         surface: "#3C4854",
+      },
+      // Font
+      font: {
+         family: {
+            body: "Inter",
+            title: "Inter",
+            code: "Inter",
+         }
+      },
+      // Breakpoints
+      screens: {
+         'xs':  '320px',
+         'sm':  '480px',
+         'md':  '768px',
+         'lg':  '992px',
+         'xl':  '1200px',
+         '2xl': '1440px',
+         '3xl': '1920px',
+      },
+   }
+   
+}
+
+export default mist_config;
