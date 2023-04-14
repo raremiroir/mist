3bf205f0745eb03ecd9c3f9ff0f5f6a48be703ec
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-15 01:51:32 +0200

    Add default button component types

---
+++ src/lib/types/components/common.d.ts
@@ -0,0 +1,9 @@
+// BUTTON / LINK
+
+export type ButtonType = "button" | "submit" | "reset";
+export type LinkTarget = "_blank" | "_self" | "_parent" | "_top" | "";
+export type LinkRel = "alternate" | "author" | "bookmark" | "external" | "help" | "license" | "next" | "nofollow" | "noreferrer" | "noopener" | "prev" | "search" | "tag" | "noopener noreferrer" | "";
+
+export interface ButtonProps {
+   
+}
