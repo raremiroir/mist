b4dece5be47c8c79bb2667f2bf9bc473dafc7a5b
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-15 01:50:57 +0200

    Add formatters

---
+++ src/lib/utils/formatters/index.ts
@@ -0,0 +1,7 @@
+import text from "./text";
+
+const format = {
+   text: text,
+}
+
+export default format;
---
+++ src/lib/utils/formatters/text.ts
@@ -0,0 +1,7 @@
+const text = {
+   trim: (str: string) => {
+      return str.replace(/\s+/g, ' ').trim();
+   }
+}
+
+export default text;
