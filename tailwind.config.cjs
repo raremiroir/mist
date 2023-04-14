5c885fa0de03e9c7032a951c5d4d5ac591efb921
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-15 00:02:53 +0200

    Rename tailwind config and import custom theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,13 @@
+const shades = require("colorshades/tailwind");
+import mist from './src/lib/theme/constants/tw'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     './src/**/*.{html,js,svelte,ts}'
   ],
-  theme: {
-    extend: {},
-  },
+  mode: 'jit',
+  theme: mist.theme,
   plugins: [],
 }
 
