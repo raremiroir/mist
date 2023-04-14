a8f9f1e54e1746294db9dd8ab3cc7fbbdeb1243d
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-15 01:51:10 +0200

    Trim result

--- src/lib/theme/generator/variant.ts
+++ src/lib/theme/generator/variant.ts
@@ -1,4 +1,5 @@
 import type { MistColorProp, MistVariantProp } from "$src/lib/types/theme";
+import format from "$src/lib/utils/formatters";
 import theme from "../mist-theme";
 
 const generateVariant = (
@@ -13,43 +14,45 @@ const generateVariant = (
       return;
    // If color is not custom, return variant styles
    } else {
+      let result = ''; 
       switch (variant) {
          // FILL
          case 'fill':
-            return ` ${theme.colors.bg[color].default} ${theme.properties.font.color.on[color]} !border-none
+            result = ` ${theme.colors.bg[color].default} ${theme.properties.font.color.on[color]} !border-none
                      ${hover ? theme.colors.bg[color].hover : ''}
                      ${active ? theme.colors.bg[color].active : ''}`;
          // OUTLINE
          case 'outline':
-            return ` ${theme.colors.border[color].default} ${theme.colors.text[color].default} bg-transparent
+            result = ` ${theme.colors.border[color].default} ${theme.colors.text[color].default} bg-transparent
                      ${hover ? `${theme.colors.bg[color].hover_alt} ${theme.colors.text[color].hover} ${theme.colors.border[color].hover}` : ''}
                      ${active ? `${theme.colors.bg[color].active_alt} ${theme.colors.text[color].active} ${theme.colors.border[color].active}` : ''}`;
          // GHOST
          case 'ghost':
-            return ` ${theme.colors.bg[color].default_alt} ${theme.colors.border[color].default} ${theme.colors.text[color].default}
+            result = ` ${theme.colors.bg[color].default_alt} ${theme.colors.border[color].default} ${theme.colors.text[color].default}
                      ${hover ? `${theme.colors.bg[color].hover_alt} ${theme.colors.border[color].hover} ${theme.colors.text[color].hover}` : ''}
                      ${active ? `${theme.colors.bg[color].active_alt} ${theme.colors.border[color].active} ${theme.colors.text[color].active}` : ''}`;
          // SOFT
          case 'soft':
-            return ` ${theme.colors.bg[color].default_alt} ${theme.colors.text[color].default} !border-none
+            result = ` ${theme.colors.bg[color].default_alt} ${theme.colors.text[color].default} !border-none
                      ${hover ? `${theme.colors.bg[color].hover_alt} ${theme.colors.text[color].hover}` : ''}
                      ${active ? `${theme.colors.bg[color].active_alt} ${theme.colors.text[color].active}` : ''}`;
          // MINIMAL
          case 'minimal':
-            return ` bg-transparent border-transparent
+            result = ` bg-transparent border-transparent
                      ${hover ? `${theme.colors.bg[color].hover_alt} ${theme.colors.text[color].hover}` : ''}
                      ${active ? `${theme.colors.bg[color].active_alt} ${theme.colors.text[color].active}` : ''}`;
          // LINK
          case 'link':
-            return ` bg-transparent border-transparent
+            result = ` bg-transparent border-transparent
                      ${hover ? `${theme.colors.bg[color].hover_alt} ${theme.colors.text[color].hover}` : ''}
                      ${active ? `${theme.colors.bg[color].active_alt} ${theme.colors.text[color].active}` : ''}`;
          // DEFAULT
          default:
-            return `bg-surface-200 text-black border-transparent
+            result = `bg-surface-200 text-black border-transparent
                   ${hover ? `hover:bg-surface-300` : ''}
                   ${active ? `active:bg-surface-400` : ''}`;
       }
+      return format.text.trim(result);
    }
 }
 
