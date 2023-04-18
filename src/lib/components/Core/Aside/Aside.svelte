ae762ccdb398c6052b3479cc6054625031d402cb
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-19 00:16:15 +0200

    Add basic aside component

---
+++ src/lib/components/Core/Aside/Aside.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+   
+   export let width: string = 'w-1/4';
+
+   let klass:string = '';
+   export { klass as class };
+
+</script>
+
+<aside class="{width} {klass}">
+   <slot/>
+</aside>
