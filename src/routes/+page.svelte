68c6332768760e1242278ff1c8bd3b192f5ffad2
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-19 01:40:30 +0200

    Add test content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
 	import { boxGen } from '$lib/theme/generator';
-	import { Button, Text, Title, Section, Page } from '$lib/components';
+	import { Button, Text, Title, Section, Page, Iconify, Accordeon, AccordeonItem } from '$lib/components';
 </script>
 
 <div class="foo">
@@ -19,11 +19,22 @@
 		</Text>
 		<Text span>
          This is a <b>span.</b>
+			<Iconify icon="material-symbols:home" />
 		</Text>
       <Text quote author="Miro Storm">
          This is a <b>quote.</b>
       </Text>
 
 		<Button>Button</Button>
+
+		<Accordeon>
+			<AccordeonItem title="something 1" id={1}>
+				Something 1
+			</AccordeonItem>
+			<AccordeonItem title="something 2" id={2}>
+				Something 2
+			</AccordeonItem>
+		</Accordeon>
+
 	</Section>
 </Page>
