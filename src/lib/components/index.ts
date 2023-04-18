a5ff923b0f5e7cbc98ad19b81bb421977d0591d5
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-19 00:17:09 +0200

    Export new components

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,6 +1,21 @@
-/*=========================================
-              ▄▀▀ ▄▀▀▄ ▄▀▀▄ ▄▀▀
-              █   █  █ █▀▀▄ █▀▀
-▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀  ▀▀  ▀  ▀  ▀▀▀▀▀▀▀▀▀▀▀▀▀▀
-=========================================*/
+/*█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
+  █           ▄▀▀ ▄▀▀▄ ▄▀▀▄ ▄▀▀           █
+  █           █   █  █ █▀▀▄ █▀▀           █
+  █▄▄▄▄▄▄▄▄▄▄▄█▀▀  ▀▀  ▀  ▀  ▀█▄▄▄▄▄▄▄▄▄▄▄█*/
 export { default as AppLayout } from './Core/AppLayout/AppLayout.svelte'
+export { default as Header } from './Core/Header/Header.svelte'
+export { default as Aside } from './Core/Aside/Aside.svelte'
+
+/*█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
+  █    ▄▀▀ ▄▀▀▄ ▄▀▄▀▄ ▄▀▄▀▄ ▄▀▀▄ █▀▀▄     █
+  █    █   █  █ █ ▀ █ █ ▀ █ █  █ █  █     █
+  █▄▄▄▄█▀▀  ▀▀  ▀   ▀ ▀   ▀  ▀▀  ▀  █▄▄▄▄▄█*/
+export { default as Button } from './Common/Button/Button.svelte'
+
+/*█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
+  █            ▀█▀ ▄▀▀ ▀▄▀ ▀█▀            █
+  █             █  █▀▀  █   █             █
+  █▄▄▄▄▄▄▄▄▄▄▄▄▄█   ▀▀ ▀ ▀  █▄▄▄▄▄▄▄▄▄▄▄▄▄█*/
+export { default as Text } from './Text/Text/Text.svelte'
+export { default as Title } from './Text/Title/Title.svelte'
+
