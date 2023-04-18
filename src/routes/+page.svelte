ca15d2ce7ccfabab4565124c1df2d2f3677cecb1
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-19 00:53:52 +0200

    Add test content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,29 @@
 <script>
-   import { boxGen } from "$lib/theme/generator";
-   import { Button, Text, Title } from "$lib/components"
+	import { boxGen } from '$lib/theme/generator';
+	import { Button, Text, Title, Section, Page } from '$lib/components';
 </script>
 
 <div class="foo">
-   <h1 class="bar">Page</h1>
-   <div>smthni</div>
+	<h1 class="bar">Page</h1>
+	<div>smthni</div>
 </div>
 
-<Button>Button</Button>
+<Page>
+	<Section title="title" subtitle="eblewiojl">
+		<Text>
+         This is a paragraph. <br/>
+			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas quidem obcaecati ex cumque
+			illum, harum odit dolore facere possimus amet saepe corporis omnis itaque esse totam qui
+			quibusdam sequi aperiam commodi placeat quia nemo. Inventore facilis reiciendis quo eius
+			labore.
+		</Text>
+		<Text span>
+         This is a <b>span.</b>
+		</Text>
+      <Text quote author="Miro Storm">
+         This is a <b>quote.</b>
+      </Text>
+
+		<Button>Button</Button>
+	</Section>
+</Page>
