ccffecbe430f35d3afd93e6e9666047558541f56
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-18 17:25:25 +0200

    Move properties to base variables

---
+++ src/lib/theme/variables/base.ts
@@ -0,0 +1,40 @@
+import type { ThemeConstants } from "../types/base";
+import type { MistColors } from "../types/properties/color";
+
+export const themeConstants: ThemeConstants = {
+   colors: {
+      shades: [50, 100, 200, 300, 400, 500, 600, 700, 800, 900],
+      types: {
+         base: [
+            "primary",
+            "secondary",
+            "tertiary",
+            "success",
+            "warning",
+            "error",
+            "surface"
+         ],
+         extend: [
+            "surface_alt",
+            "surface_dark"
+         ],
+      }
+   },
+   variants: {
+      types: [
+         "fill",
+         "outline",
+         "ghost",
+         "soft",
+         "minimal",
+         "link",
+         "unstyled"
+      ]
+   },
+   sizes: ["xs", "sm", "md", "lg", "xl", "2xl"],
+   types: ["text", "btn", "card"],
+   borders: ["none", "default", "tile", "circle"],
+   shadows: ["none", "default", "bevel", "bevel-inner"],
+}
+
+export const allColors: MistColors[] = [...themeConstants.colors.types.base, ...themeConstants.colors.types.extend];
