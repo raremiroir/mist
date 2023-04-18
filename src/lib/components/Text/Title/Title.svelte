c2894b478f5ed208e80aa5a2f1e6453f0a8ab376
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-18 22:17:24 +0200

    Add Title component

---
+++ src/lib/components/Text/Title/Title.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import type { MistSize } from "$src/lib/theme/types/properties";
+	import title from "./title";
+	import type { TitleType } from "./title";
+
+   // Can be any heading
+   export let h1: boolean = false;
+   export let h2: boolean = false;
+   export let h3: boolean = false;
+   export let h4: boolean = false;
+   export let h5: boolean = false;
+   export let h6: boolean = false;
+   // Make it a span
+   export let fake: boolean = false;
+   // Pick a size (sm, md, lg, xl, 2xl)
+   export let size:MistSize = '2xl'
+   // Override color
+   export let color: string = '';
+   // Override weight
+   export let weight: string = '';
+
+   let type: TitleType = 
+        h1? 'h1' : h2? 'h2' : h3? 'h3' 
+      : h4? 'h4' : h5? 'h5' : h6? 'h6' 
+      : 'h1';
+   let compType = fake? 'span' : type;
+
+
+   let klass:string = '';
+   export { klass as class };
+
+   let sizeClass: string = "text-4xl";
+   let weightClass: string = "font-bold";
+
+   $: sizeClass = title.class.size({type, size});
+   $: colorClass = color ? color : title.class.color(type);
+   $: weightClass = weight ? weight : title.class.weight(type);
+</script>
+
+<svelte:element 
+   this={compType}
+   class="font-title {sizeClass} {colorClass} {weightClass} {klass} text-title-">
+   <slot>Title</slot>
+</svelte:element>
---
+++ src/lib/components/Text/Title/title.ts
@@ -0,0 +1,92 @@
+import type { MistSize } from "$src/lib/theme/types/properties";
+
+export type TitleType = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+
+export const title = {
+   class: {
+      size: ( props: { type: TitleType, size: MistSize } = {
+         type: 'h1',
+         size: 'md'
+      }) => {
+         const { type, size } = props;
+         switch (type) {
+            // H1
+            case 'h1': switch (size) {
+               case 'xs': return 'text-title-lg';
+               case 'sm': return 'text-title-xl';
+               case 'md': return 'text-title-2xl';
+               case 'lg': return 'text-title-3xl';
+               case 'xl': return 'text-title-4xl';
+               case '2xl': return 'text-title-5xl';
+            }
+            // H2
+            case 'h2': switch (size) {
+               case 'xs': return 'text-title-md';
+               case 'sm': return 'text-title-lg';
+               case 'md': return 'text-title-xl';
+               case 'lg': return 'text-title-2xl';
+               case 'xl': return 'text-title-3xl';
+               case '2xl': return 'text-title-4xl';
+            }
+            // H3
+            case 'h3': switch (size) {
+               case 'xs': return 'text-title-sm';
+               case 'sm': return 'text-title-md';
+               case 'md': return 'text-title-lg';
+               case 'lg': return 'text-title-xl';
+               case 'xl': return 'text-title-2xl';
+               case '2xl': return 'text-title-3xl';
+            }
+            // H4
+            case 'h4': switch (size) {
+               case 'xs': return 'text-title-xs';
+               case 'sm': return 'text-title-sm';
+               case 'md': return 'text-title-md';
+               case 'lg': return 'text-title-lg';
+               case 'xl': return 'text-title-xl';
+               case '2xl': return 'text-title-2xl';
+            }
+            // H5
+            case 'h5': switch (size) {
+               case 'xs': return 'text-title-2xs';
+               case 'sm': return 'text-title-xs';
+               case 'md': return 'text-title-sm';
+               case 'lg': return 'text-title-md';
+               case 'xl': return 'text-title-lg';
+               case '2xl': return 'text-title-xl';
+            }
+            // H6
+            case 'h6': switch (size) {
+               case 'xs': return 'text-title-3xs';
+               case 'sm': return 'text-title-2xs';
+               case 'md': return 'text-title-xs';
+               case 'lg': return 'text-title-sm';
+               case 'xl': return 'text-title-md';
+               case '2xl': return 'text-title-lg';
+            }
+         }
+      },
+      color: ( type: TitleType = 'h1' ) => {
+         switch (type) {
+            case 'h1': return 'text-primary-600';
+            case 'h2': return 'text-primary-600';
+            case 'h3': return 'text-primary-700';
+            case 'h4': return 'text-primary-700';
+            case 'h5': return 'text-primary-800';
+            case 'h6': return 'text-primary-900';
+         }
+      },
+      weight: ( type: TitleType = 'h1' ) => {
+         switch (type) {
+            case 'h1': return 'font-bold';
+            case 'h2': return 'font-bold';
+            case 'h3': return 'font-bold';
+            case 'h4': return 'font-medium';
+            case 'h5': return 'font-medium';
+            case 'h6': return 'font-medium';
+         }
+      }
+   }
+}
+
+export default title;
