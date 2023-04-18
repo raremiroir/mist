b1bb9d06ab96345319ebc932174529262b6b70b7
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-19 00:16:29 +0200

    Add base tailwind styles

---
+++ src/lib/theme/tailwind/styles.ts
@@ -0,0 +1,32 @@
+export const mWidths = 'w-[99%] xs:w-[95%] sm:w-11/12 md:w-5/6 lg:w-4/5 xl:w-3/4 2xl:w-2/3 3xl:w-1/2'
+
+export const mFlex = {
+   center: ' flex items-center justify-center',
+   start: ' flex items-center justify-start',
+   end: ' flex items-center justify-end',
+   between: ' flex items-center justify-between',
+   around: ' flex items-center justify-around',
+   evenly: ' flex items-center justify-evenly',
+   col: ' flex flex-col',
+   colCenter: ' flex flex-col items-center justify-center',
+   colStart: ' flex flex-col items-center justify-start',
+   colEnd: ' flex flex-col items-center justify-end',
+   colBetween: ' flex flex-col items-center justify-between',
+   colAround: ' flex flex-col items-center justify-around',
+   colEvenly: ' flex flex-col items-center justify-evenly',
+}
+
+export const mTransition = {
+   default: ' transition duration-300 ease-in-out',
+   slow: ' transition duration-500 ease-in-out',
+   fast: ' transition duration-100 ease-in-out',
+}
+
+// All mist-tailwind styles exported
+const mtw = {
+   flex: mFlex,
+   transition: mTransition,
+   widths: mWidths,
+}
+
+export default mtw;
