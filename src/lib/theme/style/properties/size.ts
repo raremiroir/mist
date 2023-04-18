96d9037e16974da18036ce06cab1f0aa24c27521
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-18 22:18:48 +0200

    Edit sizes

--- src/lib/theme/style/properties/size.ts
+++ src/lib/theme/style/properties/size.ts
@@ -4,7 +4,7 @@ import type { MistThemeSizes } from "../../types/properties/size";
 
 export const mistThemeSizes: MistThemeSizes = {
    text: {
-      xs:    ` font-size: 0.75rem;`,
+      xs:    ` font-size: 0.5rem;`,
       sm:    ` font-size: 0.875rem;`,
       md:    ` font-size: 1rem;`,
       lg:    ` font-size: 1.125rem;`,
@@ -12,9 +12,9 @@ export const mistThemeSizes: MistThemeSizes = {
       '2xl': ` font-size: 1.5rem;`,
    },
    btn: {
-      xs:    ` padding: 0.25rem 0.5rem;`,
-      sm:    ` padding: 0.375rem 0.75rem;`,
-      md:    ` padding: 0.5rem 1rem;`,
+      xs:    ` padding: 0.25rem 0.5rem 0.4rem;`,
+      sm:    ` padding: 0.375rem 0.75rem 0.5rem;`,
+      md:    ` padding: 0.5rem 1rem 0.8rem;`,
       lg:    ` padding: 0.625rem 1.25rem;`,
       xl:    ` padding: 0.75rem 1.5rem;`,
       '2xl': ` padding: 0.875rem 1.75rem;`,
