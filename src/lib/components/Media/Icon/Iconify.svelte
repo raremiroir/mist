89b258dfa082a8ebefc428c648c094de1db9de9d
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-19 01:40:18 +0200

    Add Icon component base

---
+++ src/lib/components/Media/Icon/Iconify.svelte
@@ -0,0 +1,16 @@
+<script lang='ts'>
+   // IMPORT COMPONENTS
+   import Icon from '@iconify/svelte';
+
+   export let icon:string;
+   
+   export let size:string = 'w-6'
+   export let color:string = 'text-primary fill-primary'
+
+   let klass:string = ''
+   export { klass as class };
+</script>
+
+<div class="">
+   <Icon {icon} class='{color} {size} h-auto {klass}' />
+</div>
