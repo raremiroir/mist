06cff237c898b9aad1bb7831dbccbe670c509f48
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-18 17:25:39 +0200

    Add hsl formatting

--- src/lib/utils/formatters/color.ts
+++ src/lib/utils/formatters/color.ts
@@ -22,6 +22,39 @@ const formatColor = {
         g: parseInt(result[2], 16),
         b: parseInt(result[3], 16)
       } : { r: 0, g: 0, b: 0 };
+    },
+    toHsl: (hex:string) => {
+      const rgb = formatColor.hex.toRgb(hex);
+      const r = rgb.r / 255, g = rgb.g / 255, b = rgb.b / 255;
+      const max = Math.max(r, g, b), min = Math.min(r, g, b);
+      let h:any, s:any, l = (max + min) / 2;
+    
+      if(max == min) {
+        h = s = 0; // achromatic
+      } else {
+        const d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        
+        switch(max){
+          case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+          case g: h = (b - r) / d + 2; break;
+          case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
+      }
+      return {h: h * 360, s: s * 100, l: l * 100};
+    }
+  },
+  hsl: {
+    toHex: (h:number, s:number, l:number) => {
+      l /= 100;
+      const a = s * Math.min(l, 1 - l) / 100;
+      const f = (n:number) => {
+        const k = (n + h / 30) % 12;
+        const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+        return Math.round(255 * color).toString(16).padStart(2, '0');   // convert to Hex and prefix "0" if needed
+      };
+      return `#${f(0)}${f(8)}${f(4)}`;
     }
   }
 };
