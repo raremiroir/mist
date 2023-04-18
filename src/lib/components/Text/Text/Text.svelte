ca158e14e32305a3d9c1456c96bb8c1664231f3f
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-18 22:20:14 +0200

    Add Text component

---
+++ src/lib/components/Text/Text/Text.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+   // Can be p or span
+   export let type: 'p' | 'span' = "p";
+
+   let klass:string = '';
+   export { klass as class };
+</script>
+
+<svelte:element 
+   this={type}
+   class="font-body {klass}">
+   <slot/>
+</svelte:element>
