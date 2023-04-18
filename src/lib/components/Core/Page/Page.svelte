420f71bc904c45169405c410e0b7e854d1fea6a7
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-19 00:38:10 +0200

    Add Page component

---
+++ src/lib/components/Core/Page/Page.svelte
@@ -0,0 +1,12 @@
+<script lang='ts'>
+   // IMPORT TYPES
+
+   // IMPORT COMPONENTS
+
+   let klass:string = ''
+   export { klass as class };
+</script>
+
+<div class='w-full {klass}'>
+  <slot/>
+</div>	
