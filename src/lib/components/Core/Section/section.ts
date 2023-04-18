acf841f1fda1fdb88e3e58bbb93f65aa7f942306
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-19 00:54:37 +0200

    Add types for section colors

--- src/lib/components/Core/Section/section.ts
+++ src/lib/components/Core/Section/section.ts
@@ -1,1 +1,19 @@
 export type SectionTitleBorder = 'none'|'bottom'|'left'|'box';
+
+
+export type SectionThemes = 
+   'color' 
+   | 'transparent' 
+   | 'illustrated' 
+   | 'parallax';
+export type SectionColors = 
+   'default'
+   | 'darker'
+   | 'lighter'
+   | 'primary'
+   | 'secondary'
+   | 'success'
+   | 'info'
+   | 'warning'
+   | 'danger'
+   | 'transparent';
