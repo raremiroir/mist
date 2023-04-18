28fa13b9c827cfe56d8f913e8e8522627d8666c0
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-19 00:52:55 +0200

    Add quote prop

--- src/lib/components/Text/Text/Text.svelte
+++ src/lib/components/Text/Text/Text.svelte
@@ -1,13 +1,37 @@
 <script lang="ts">
-   // Can be p or span
-   export let type: 'p' | 'span' = "p";
+   // Set span or quote to true to change the element type
+   // and to disable p
+   export let span:boolean = false;
+   export let quote:boolean = false;
+   export let author:string = '';   // only for quote
+   
+   let compType = 
+      span ? 'span'
+      : quote ? 'blockquote'
+      : 'p';
 
    let klass:string = '';
    export { klass as class };
+
+   let compClass = 'font-body';
+   compClass += quote ? ` 
+      border-l-4 border-primary-600/40 
+      ml-4 my-2 pl-4 text-xl
+      text-primary-800
+   ` : ''
+
+   const authorClass = "pl-4 mt-2 text-lg font-semibold italic text-primary-700/80";
 </script>
 
 <svelte:element 
-   this={type}
-   class="font-body {klass}">
+   this={compType}
+   class="{compClass} {klass}">
+   
    <slot/>
+
+   {#if quote}
+      <div class={authorClass}>
+         ~ {author}
+      </div>
+   {/if}
 </svelte:element>
