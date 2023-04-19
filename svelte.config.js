0802264fa320ef6f57f7808de69dda4011dfe69e
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-19 12:26:11 +0200

    Add alias to mist

--- svelte.config.js
+++ svelte.config.js
@@ -19,6 +19,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		alias: {
+			$mist: './mist',
 			$src: './src',
 			$components: './src/components',
 			$lib: './src/lib'
