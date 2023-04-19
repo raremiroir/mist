1210513d4c09defb786bee3fb0919c713793ed8c
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-19 13:33:15 +0200

    Move to types folder

--- mist/types/theme.d.ts
+++ mist/types/theme.d.ts
@@ -1,7 +1,7 @@
-import type { MistBorderType } from "./properties/border/border";
-import type { ColorStyleState, MistColor, MistColors, MistShade, MistVariantStates } from "./properties/color/color";
-import type { MistShadowTypes } from "./properties/shadow/shadow";
-import type { MistVariant } from "./properties/variant/variant";
+import type { MistBorderType } from "../theme/properties/border/border";
+import type { MistColor, MistColors, MistShade, MistVariantStates } from "../theme/properties/color/color";
+import type { MistShadowType } from "../theme/properties/shadow/shadow";
+import type { MistVariant } from "../theme/properties/variant/variant";
 
 export type MistSize = "xs" | "sm" | "md" | "lg" | "xl" | "2xl";
 export type MistComponentType = "btn" | "card" | "text";
@@ -28,6 +28,6 @@ export type MistThemeProperties = {
       sizes: MistSize[],
       types: MistComponentType[],
       borders: MistBorderType[],
-      shadows: MistShadowTypes[],
+      shadows: MistShadowType[],
    }
 };
